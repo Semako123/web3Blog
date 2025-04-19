0473f03d17f469898c43bfb500228345fec24027
semako123 <zosusemenoch@gmail.com>
2025-04-19 09:09:11 +0100

    feat: Adds connect wallet button to home route

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,15 @@
+import ConnectWalletButton from "@/components/ConnectWalletButton";
 import Link from "next/link";
 
 export default function Home() {
+
   return (
     <div className="w-full">
       <h1>
         Hey, this is the home page of the blog app. You can create a blog post
-        by going to the <Link href="/create">create</Link> page.
+        by going to the <Link href="/create" className="text-blue-300">create</Link> page.
       </h1>
+      <ConnectWalletButton/>
     </div>
   );
 }
