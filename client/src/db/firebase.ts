68015c5eeae80cd66680061816c1b445c86d23b8
semako123 <zosusemenoch@gmail.com>
2025-04-20 03:44:28 +0100

    fix: make firebase key secret Removes server-only check

--- client/src/db/firebase.ts
+++ client/src/db/firebase.ts
@@ -1,4 +1,3 @@
-import "server-only"
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -9,19 +8,19 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBcH_G_qFtVhxCLWXM04hr87-AcaLvs8KI",
-  authDomain: "web3blog-dd90d.firebaseapp.com",
-  projectId: "web3blog-dd90d",
-  storageBucket: "web3blog-dd90d.firebasestorage.app",
-  messagingSenderId: "993217149229",
-  appId: "1:993217149229:web:1a5db5be928c74a9f3fba4",
-  measurementId: "G-70W7N07ZC1",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
-const db = getFirestore(app)
+const db = getFirestore(app);
 
-export default db
+export default db;
