8fcaf3802f162ea5e3fd1c747c506ddc6f34dbee
semako123 <zosusemenoch@gmail.com>
2025-04-21 18:25:43 +0100

    style: Update font type

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeProvider";
 import WagmiProvider from "@/context/WagmiProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const interSans = Inter({
+  variable: "--font-inter-sans",
   subsets: ["latin"],
 });
 
@@ -26,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${interSans.variable} antialiased`}>
         <WagmiProvider>
           <ThemeProvider
             attribute="class"
